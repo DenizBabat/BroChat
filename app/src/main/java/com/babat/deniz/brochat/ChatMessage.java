9bbb8ce8d42b4994a228889e6cabc22bbe74751b
Deniz Babat <dbabat@gtu.edu.tr>
2017-12-11 19:54:31 +0300

    * Bugs was solute * One more person chat together * Emoji was added

--- app/src/main/java/com/babat/deniz/brochat/ChatMessage.java
+++ app/src/main/java/com/babat/deniz/brochat/ChatMessage.java
@@ -8,7 +8,6 @@ import java.util.Date;
  */
 
 public class ChatMessage {
-
     private String messageText;
     private String messageUser;
     private long messageTime;
@@ -16,6 +15,7 @@ public class ChatMessage {
     public ChatMessage(String messageText, String messageUser) {
         this.messageText = messageText;
         this.messageUser = messageUser;
+
         messageTime = new Date().getTime();
     }
 
@@ -26,20 +26,20 @@ public class ChatMessage {
         return messageText;
     }
 
-    public String getMessageUser() {
-        return messageUser;
-    }
-
-    public long getMessageTime() {
-        return messageTime;
-    }
-
     public void setMessageText(String messageText) {
         this.messageText = messageText;
     }
 
+    public String getMessageUser() {
+        return messageUser;
+    }
+
     public void setMessageUser(String messageUser) {
         this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
     }
 
     public void setMessageTime(long messageTime) {
